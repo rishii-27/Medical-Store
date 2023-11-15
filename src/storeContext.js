d96fa85740c9000da525abdec501e975ef3b9a4b
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2023-11-15 12:56:53 +0530

    List eloads on refresh: Data Fetched

--- src/storeContext.js
+++ src/storeContext.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const StoreContext = React.createContext({});
 
@@ -6,7 +6,37 @@ export const StoreContextProvider = (props) => {
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
 
+  useEffect(() => {
+    // Fetch data from the API
+    fetch("https://crudcrud.com/api/18d5bbfb61144c829b8324e6d4feb3b9/items", {
+      method: "GET",
+    })
+      .then((response) => response.json()) // Parse the response body as JSON
+      .then((data) => {
+        setItems(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   const addMedsToList = (item) => {
+    fetch(`https://crudcrud.com/api/18d5bbfb61144c829b8324e6d4feb3b9/items`, {
+      method: "POST",
+      body: JSON.stringify(item),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Failed to add medicine. Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+
     setItems([...items, item]);
   };
 
